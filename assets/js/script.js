d769a5077307005bb824d20a485c9a6b94ea524d
n1zhon1 <kristineetsitty@gmail.com>
2021-09-11 20:17:44 -0700

    added main variables to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,7 @@ var searchFormEl = document.querySelector("#user-form");
 var cityNameInputEl = document.querySelector("#city-name");
 var currentWeatherContainerEl = document.querySelector("#current-weather");
 var fiveDaySearchTerm = document.querySelector("#five-day");
+
+var getCurrentWeather = function(city) {
+    var 
+}
