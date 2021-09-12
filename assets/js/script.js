876f6dcc57dc5dfdd8858aae1341e530fcaaa288
n1zhon1 <kristineetsitty@gmail.com>
2021-09-11 19:06:27 -0700

    completed html columns - updated css and js files

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+var searchFormEl = document.querySelector("#user-form");
+var cityNameInputEl = document.querySelector("#city-name");
+var currentWeatherContainerEl = document.querySelector("#current-weather");
+var fiveDaySearchTerm = document.querySelector("#five-day");
